c5f3b8e5d0ca3463fbefa847e8407ff1c8048bdd
Avery <111928699+avery-kb@users.noreply.github.com>
2025-11-03 23:45:16 -0500

    Update version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="log_colors",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     description="A colorful logging library for Python",
     long_description=long_description,
