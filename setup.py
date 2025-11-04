67de93f245c54bb013b1630098904f6d43722fcd
avery <akuhnbrooks@gmail.com>
2025-11-03 23:30:18 -0500

    Updates for pypi upload

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name="log_colors",
     version="0.1.0",
     packages=find_packages(),
     description="A colorful logging library for Python",
-    long_description=open("README.md").read(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="avery-kb",
     install_requires=[],
     python_requires=">=3.7",
